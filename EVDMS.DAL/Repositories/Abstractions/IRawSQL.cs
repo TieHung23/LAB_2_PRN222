4ae67f42ae5d205792cc93f5f0acf82ab0d327f8
TieHung23 <hungptse183180@fpt.edu.vn>
2025-10-17 17:16:29 +0700

    [FIX BUG] Error at <, clean code

--- EVDMS.DAL/Repositories/Abstractions/IRawSQL.cs
+++ EVDMS.DAL/Repositories/Abstractions/IRawSQL.cs
@@ -1,14 +1,5 @@
-using System;
-using Dapper;
-using EVDMS.DAL.Database;
-using EVDMS.DAL.Repositories.Abstractions;
-using EVDMS.DAL.Response;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-
 namespace EVDMS.DAL.Repositories.Abstractions;
 
 public interface IRawSQL
 {
-    Task<List<RevenueDataDTOs>> ExecRawSQLAsync(string sql);
 }
